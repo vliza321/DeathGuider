c21cef06e7649f749b2ee88b3f3490c070914e7c
YSL-RyuDo <lys020207@naver.com>
2024-07-19 22:02:16 +0900

    스크립트
    스크립트 층 생성
    층 버튼 클릭 시 죄수 데이터 볼 수 있는 ui

---
+++ Assets/Script/LobbyScene/UpgradeFloor.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpgradeFloor : MonoBehaviour
+{
+    public GameObject floorPrefab; // Ãþ ÇÁ¸®Æé
+    public Transform parentTransform; // ÃþÀÇ ºÎ¸ð Æ®·£½ºÆû ¼³Á¤
+    private int floorCount = 1; // Ãþ ¼ö
+
+    public void CreateFloor()
+    {
+        // Ãþ »ý¼º
+        GameObject newFloor = Instantiate(floorPrefab, parentTransform);
+
+        // Ãþ ³ôÀÌ °è»ê
+        RectTransform rectTransform = newFloor.GetComponent<RectTransform>();
+        float FloorHeight = rectTransform.rect.height;
+
+        // Ãþ À§Ä¡ ¼³Á¤
+        rectTransform.anchoredPosition = new Vector2(0, floorCount * FloorHeight);
+
+        // Ãþ ÀÌ¸§ ¼³Á¤
+        newFloor.name = "Floor " + (floorCount + 1) + "Ãþ";
+
+        // Ãþ ¼ö Áõ°¡
+        floorCount++;
+    }
+}
