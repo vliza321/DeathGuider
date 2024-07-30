3beccdcd9041adb6ebfa4a34d7dbbeb5279e098b
YSL-RyuDo <lys020207@naver.com>
2024-07-30 17:06:49 +0900

    버스 수감자 완성
    버스 수감자 리스트 재배열
    버스 수감자 정보 저장 공간 만들기

--- Assets/Script/LobbyScene/BusPrisonerUI.cs
+++ Assets/Script/LobbyScene/BusPrisonerUI.cs
@@ -9,15 +9,35 @@ public class BusPrisonerUI : MonoBehaviour
     public RectTransform content; // Content 참조
     public DateSystem dateSystem;
     public Button changeDateButton;
+    public Button[] rejectButtons; // 각 죄수에 대한 Reject 버튼 배열
+    public Button[] transferButtons; // 각 죄수에 대한 Transfer 버튼 배열
 
     public Text[] nameTexts; // 6개의 Text UI 요소 배열
+    public Text[] hpTexts; // 6개의 HP Text UI 요소 배열
+    public Text[] proficiencyTexts; // 6개의 숙련도 Text UI 요소 배열
+    public Text[] strengthTexts; // 6개의 힘 Text UI 요소 배열
+    public Text[] crimeTexts; // 6개의 범죄 Text UI 요소 배열
+    public Image[] prisonerImages; // 각 죄수에 대한 이미지 배열 (옵션)
+    //public Image warningImage; // 공간 부족 경고 이미지
+    public GameObject warningImage;
+
     private readonly char[] name1 = new char[] { 'ㄱ', 'ㄲ', 'ㄴ', 'ㄷ', 'ㄸ', 'ㄹ', 'ㅁ', 'ㅂ', 'ㅃ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅉ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ' };
     private readonly char[] name2 = new char[] { 'ㅏ', 'ㅐ', 'ㅑ', 'ㅒ', 'ㅓ', 'ㅔ', 'ㅕ', 'ㅖ', 'ㅗ', 'ㅘ', 'ㅙ', 'ㅚ', 'ㅛ', 'ㅜ', 'ㅝ', 'ㅞ', 'ㅟ', 'ㅠ', 'ㅡ', 'ㅢ', 'ㅣ' };
     private readonly char[] name3 = new char[] { '\0', 'ㄱ', 'ㄲ', 'ㄳ', 'ㄴ', 'ㄵ', 'ㄶ', 'ㄷ', 'ㄹ', 'ㄺ', 'ㄻ', 'ㄼ', 'ㄽ', 'ㄾ', 'ㄿ', 'ㅀ', 'ㅁ', 'ㅂ', 'ㅄ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ' };
 
     private readonly string[] firstNames = new string[] { "김", "이", "박", "최", "정", "강", "조", "윤", "장", "임" };
-    //private List<string> prisonerNames = new List<string> { "강병찬", "김민성", "박광희", "이상준", "이주원", "이영상", "장영규", "정재민", "진주형", "한정도" };
+    private readonly string[] crimes = new string[] { "방화", "살인", "패륜", "사기", "절도" };
+
     private List<string> currentPrisonerNames = new List<string>();
+    private List<int> currentPrisonerHPs = new List<int>();
+    private List<int> currentPrisonerProficiencies = new List<int>();
+    private List<int> currentPrisonerStrength = new List<int>();
+    private List<string> currentPrisonerCrimes = new List<string>();
+
+    public UpgradeFloor upgradeFloor; // UpgradeFloor 스크립트 참조
+    public Transform prisonerParent; // 하이어라키의 prisoner 오브젝트
+    private int currentPrisonerCount = 0; // 현재 추가된 수감자 수를 추적하는 카운터
+
 
     void Start()
     {
@@ -30,16 +50,48 @@ public class BusPrisonerUI : MonoBehaviour
             changeDateButton.onClick.AddListener(OnChangeDateButtonClick);
         }
 
+        // Reject 버튼에 리스너 추가
+        if (rejectButtons != null)
+        {
+            for (int i = 0; i < rejectButtons.Length; i++)
+            {
+                int index = i; // 로컬 변수로 인덱스 저장
+                rejectButtons[i].onClick.AddListener(() => OnRejectButtonClick(index));
+            }
+        }
+
+        // Transfer 버튼에 리스너 추가
+        if (transferButtons != null)
+        {
+            for (int i = 0; i < transferButtons.Length; i++)
+            {
+                int index = i; // 로컬 변수로 인덱스 저장
+                transferButtons[i].onClick.AddListener(() => OnTransferButtonClick(index));
+            }
+        }
+
         // 날짜 변경 이벤트 구독
         if (dateSystem != null)
         {
-            dateSystem.OnDateChanged += UpdatePrisonerNames;
-            // 초기화 시 첫 날짜에 맞는 이름 설정
-            UpdatePrisonerNames();
+            dateSystem.OnDateChanged += UpdatePrisoner;
+            // 초기화 시 첫 날짜에 맞는 이름과 체력 설정
+            UpdatePrisoner();
         }
 
         // ScrollRect의 위치를 초기화하는 코루틴 시작
         StartCoroutine(SetScrollPosition());
+
+        // UpgradeFloor 스크립트 참조 설정
+        upgradeFloor = FindObjectOfType<UpgradeFloor>();
+
+        // 경고 이미지 초기 숨김
+        if (warningImage != null)
+        {
+            warningImage.gameObject.SetActive(false);
+        }
+
+        // 하이어라키의 prisoner 오브젝트 찾기
+        prisonerParent = GameObject.Find("StoredPrisoner")?.transform;
     }
 
     void OnDestroy()
@@ -47,7 +99,7 @@ public class BusPrisonerUI : MonoBehaviour
         // 날짜 변경 이벤트 구독 해제
         if (dateSystem != null)
         {
-            dateSystem.OnDateChanged -= UpdatePrisonerNames;
+            dateSystem.OnDateChanged -= UpdatePrisoner;
         }
     }
 
@@ -66,35 +118,137 @@ public class BusPrisonerUI : MonoBehaviour
         }
     }
 
-    private void UpdatePrisonerNames()
+    void OnRejectButtonClick(int index)
     {
-        // 랜덤한 이름 생성
+        // 인덱스에 해당하는 죄수를 제거합니다.
+        if (index >= 0 && index < currentPrisonerNames.Count)
+        {
+            currentPrisonerNames.RemoveAt(index);
+            currentPrisonerHPs.RemoveAt(index);
+            currentPrisonerProficiencies.RemoveAt(index);
+            currentPrisonerStrength.RemoveAt(index);
+            currentPrisonerCrimes.RemoveAt(index);
+
+            // UI 업데이트
+            SetPrisoner();
+        }
+    }
+
+    public void OnTransferButtonClick(int index)
+    {
+        if (upgradeFloor == null || prisonerParent == null)
+        {
+            Debug.LogError("UpgradeFloor or prisonerParent is not set.");
+            return;
+        }
+
+        int floorCapacity = upgradeFloor.floorCount * 4; // 한 층당 최대 4명 수용 가능
+        int currentCapacity = prisonerParent.childCount; // 현재 층의 수감자 수
+
+        if (index >= 0 && index < currentPrisonerNames.Count)
+        {
+            if (currentCapacity >= floorCapacity)
+            {
+                // 공간 부족 시 경고 이미지 표시
+                if (warningImage != null)
+                {
+                    warningImage.SetActive(true);
+                }
+            }
+            else
+            {
+                // 공간이 충분할 경우, 수감자 정보를 하이어라키에 이동
+                GameObject prisonerObj = new GameObject("Prisoner_" + (currentPrisonerCount + 1)); // 고유 이름 설정
+                prisonerObj.transform.SetParent(prisonerParent);
+
+                StoredPrisoner prisonerScript = prisonerObj.AddComponent<StoredPrisoner>();
+                prisonerScript.prisonerName = currentPrisonerNames[index];
+                prisonerScript.hp = currentPrisonerHPs[index];
+                prisonerScript.proficiency = currentPrisonerProficiencies[index];
+                prisonerScript.strength = currentPrisonerStrength[index];
+                prisonerScript.crime = currentPrisonerCrimes[index];
+
+                // 리스트에서 제거
+                currentPrisonerNames.RemoveAt(index);
+                currentPrisonerHPs.RemoveAt(index);
+                currentPrisonerProficiencies.RemoveAt(index);
+                currentPrisonerStrength.RemoveAt(index);
+                currentPrisonerCrimes.RemoveAt(index);
+
+                // 수감자 카운터 증가
+                currentPrisonerCount++;
+
+                // UI 업데이트
+                SetPrisoner();
+            }
+        }
+    }
+
+
+    private void UpdatePrisoner()
+    {
+        // 랜덤한 이름과 정보를 생성합니다.
         currentPrisonerNames.Clear();
+        currentPrisonerHPs.Clear();
+        currentPrisonerProficiencies.Clear();
+        currentPrisonerStrength.Clear();
+        currentPrisonerCrimes.Clear();
+
         for (int i = 0; i < 6; i++)
         {
             currentPrisonerNames.Add(GenerateRandomName());
+            currentPrisonerHPs.Add(Random.Range(1, 11));
+            currentPrisonerProficiencies.Add(Random.Range(1, 11));
+            currentPrisonerStrength.Add(Random.Range(1, 11));
+            currentPrisonerCrimes.Add(crimes[Random.Range(0, crimes.Length)]);
         }
 
         // 현재 선택된 죄수 이름들로 텍스트 설정
-        SetPrisonerNames();
+        SetPrisoner();
     }
 
-    private void SetPrisonerNames()
+    private void SetPrisoner()
     {
-        // 텍스트에 현재 선택된 죄수 이름 배정
         for (int i = 0; i < nameTexts.Length; i++)
         {
-            if (nameTexts[i] != null)
+            if (i < currentPrisonerNames.Count)
             {
-                if (i < currentPrisonerNames.Count)
+                nameTexts[i].text = currentPrisonerNames[i];
+                hpTexts[i].text = "체력: " + currentPrisonerHPs[i];
+                proficiencyTexts[i].text = "숙련도: " + currentPrisonerProficiencies[i];
+                strengthTexts[i].text = "힘: " + currentPrisonerStrength[i];
+                crimeTexts[i].text = "범죄: " + currentPrisonerCrimes[i];
+
+                if (prisonerImages[i] != null)
                 {
-                    nameTexts[i].text = currentPrisonerNames[i];
+                    prisonerImages[i].gameObject.SetActive(true);
                 }
-                else
+            }
+            else
+            {
+                // 정보가 없는 경우 UI 요소를 숨깁니다.
+                nameTexts[i].text = "";
+                hpTexts[i].text = "";
+                proficiencyTexts[i].text = "";
+                strengthTexts[i].text = "";
+                crimeTexts[i].text = "";
+
+                if (prisonerImages[i] != null)
                 {
-                    // 이름이 부족한 경우 기본값 또는 빈 문자열 설정
-                    nameTexts[i].text = "이름 없음";
+                    prisonerImages[i].gameObject.SetActive(false);
                 }
+            }
+
+            // Reject 버튼 활성화/비활성화
+            if (rejectButtons[i] != null)
+            {
+                rejectButtons[i].gameObject.SetActive(i < currentPrisonerNames.Count);
+            }
+
+            // Transfer 버튼 활성화/비활성화
+            if (transferButtons[i] != null)
+            {
+                transferButtons[i].gameObject.SetActive(i < currentPrisonerNames.Count);
             }
         }
     }
@@ -102,9 +256,8 @@ public class BusPrisonerUI : MonoBehaviour
     private string GenerateRandomName()
     {
         string familyName = firstNames[Random.Range(0, firstNames.Length)];
-        char firstNameInitial1 = CreateRandomKoreanChar();
-        char firstNameInitial2 = CreateRandomKoreanChar();
-        return familyName + firstNameInitial1.ToString() + firstNameInitial2.ToString();
+        string firstName = CreateRandomKoreanChar().ToString() + CreateRandomKoreanChar().ToString();
+        return familyName + firstName;
     }
 
     private char CreateRandomKoreanChar()
@@ -119,18 +272,12 @@ public class BusPrisonerUI : MonoBehaviour
 
         int unicode = 0xAC00 + (fName * 21 * 28) + (sName * 28) + tName;
 
-        return (char)unicode;
-    }
-
-    // 리스트를 무작위로 셔플하는 메서드
-    private void ShuffleList<T>(List<T> list)
-    {
-        for (int i = list.Count - 1; i > 0; i--)
+        // 유니티가 읽지 못하는 범위의 유니코드 문자 배제
+        if (unicode < 0xAC00 || unicode > 0xD7A3)
         {
-            int j = Random.Range(0, i + 1);
-            T temp = list[i];
-            list[i] = list[j];
-            list[j] = temp;
+            unicode = 0xAC00; // 기본값으로 설정
         }
+
+        return (char)unicode;
     }
 }
