eb99d7e2131f8725700801fe956bfc508ab36fb9
장영규 <jungyk3645@naver.com>
2024-09-06 21:01:52 +0900

    TitleScene, LoadingScene
    Title, Loading Scene.
    Loading Scene은 추후 유저 데이터를 판별하기 위해 제작해놓음.
    프로토타입의 경우 무시하고 SceneLoader에서 Main씬으로의 이동으로 바꿔도 무관.

---
+++ Assets/Script/LoadingScene/SceneLoad.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneLoad : MonoBehaviour
+{
+    public Slider progressbar;
+    public Text loadtext;
+    private void Start()
+    {
+        StartCoroutine(LoadScene());
+    }
+        IEnumerator LoadScene()
+    {
+        yield return null;
+        AsyncOperation operation = SceneManager.LoadSceneAsync("Main");
+        operation.allowSceneActivation = false;
+
+        while (!operation.isDone)
+        {
+            yield return null;
+            if (progressbar.value < 0.9f)
+            {
+                progressbar.value = Mathf.MoveTowards(progressbar.value, 0.9f, Time.deltaTime);
+            }else if(operation.progress >= 0.9f)
+            {
+                progressbar.value = Mathf.MoveTowards(progressbar.value, 1f, Time.deltaTime);
+            }
+
+            if(progressbar.value >= 1f)
+            {
+                loadtext.text = "Press SpaceBar";
+            }
+
+            if(Input.GetKeyDown(KeyCode.Space)&&progressbar.value >= 1f && operation.progress >= 0.9f)
+            {
+                operation.allowSceneActivation = true;
+            }
+
+            
+        }
+    }
+}
